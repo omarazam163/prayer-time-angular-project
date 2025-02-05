1d9b07a05abc380de138eaed3da369436716c988
omar ahmed <50985259+omarazam163@users.noreply.github.com>
2025-02-05 20:19:08 +0200

    try

--- src/app/geo-service.service.ts
+++ src/app/geo-service.service.ts
@@ -2,33 +2,25 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GeoServiceService {
   private _httpClient = inject(HttpClient);
   private _platformId = inject(PLATFORM_ID);
-  
-  constructor() { }
 
+  constructor() {}
+  ip="";
   getGeoData(): Observable<any> {
-    if (isPlatformBrowser(this._platformId)) {
-      return this._httpClient.get<{ ip: string }>('https://api.ipify.org?format=json').pipe(
-        switchMap((res) => {
-          const userIp = res.ip;
-          return this._httpClient.get(
-            `https://apiip.net/api/check?ip=${userIp}&accessKey=fdcd50df-6653-49e6-87cb-ee2660e7c95c`
-          );
-        })
-      );
-    } else {
-      // Running on the server, use a default IP
-      return this._httpClient.get(
-        `https://apiip.net/api/check?ip=8.8.8.8&accessKey=fdcd50df-6653-49e6-87cb-ee2660e7c95c`
-      );
-    }
+    this._httpClient.get('https://api.ipify.org?format=json').subscribe((res: any) => {
+      this.ip = res.ip
+    })
+    return this._httpClient.get(
+              `https://apiip.net/api/check?ip=${this.ip}&accessKey=fdcd50df-6653-49e6-87cb-ee2660e7c95c`
+            );
   }
 }
+
